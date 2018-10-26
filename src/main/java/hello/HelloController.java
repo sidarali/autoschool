967f0d6ad9b3dbceb362fcff4bbd6703e18c0b27
Alexander Sidorenko <sidarali@gmail.com>
2018-10-26 04:13:22 +0300

    webapp initial spring boot app

---
+++ src/main/java/hello/HelloController.java
@@ -0,0 +1,17 @@
+package hello;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+    private static final Logger log = LoggerFactory.getLogger(HelloController.class);
+    @RequestMapping("/")
+    public String index() {
+        log.info("logger hi");
+        return "Greetings from Spring Boot!";
+    }
+    
+}
